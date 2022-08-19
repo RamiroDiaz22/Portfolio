fb685e369f68bbcd167de5c2d57157aab342a920
RamiroDiaz22 <diazbravoramiro@gmail.com>
2022-08-19 00:58:15 -0300

    Actualizaciòn Nav 2.0

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,6 +6,53 @@ import { RiServiceLine } from "react-icons/ri";
 
 function Nav() {
   const [activeNav, setActiveNav] = useState("#home");
+
+  window.addEventListener("scroll", function () {
+    let position1 = document.getElementById("home").getBoundingClientRect().top;
+    let position2 = document
+      .getElementById("about")
+      .getBoundingClientRect().top;
+    let position3 = document
+      .getElementById("experience")
+      .getBoundingClientRect().top;
+    let position4 = document
+      .getElementById("services")
+      .getBoundingClientRect().top;
+    let position5 = document
+      .getElementById("contact")
+      .getBoundingClientRect().top;
+
+    if (position1 < 150 && position1 > -150) {
+      setActiveNav("#home");
+    }
+    if (position2 < 150 && position2 > -150) {
+      setActiveNav("#about");
+    }
+    if (position3 < 150 && position3 > -150) {
+      setActiveNav("#experience");
+    }
+    if (position4 < 150 && position4 > -150) {
+      setActiveNav("#services");
+    }
+    if (position5 < 150 && position5 > -150) {
+      setActiveNav("#contact");
+    }
+
+    // position1 === 0 ? (
+    //   setActiveNav("#home")
+    // ) : position2 === 0 ? (
+    //   setActiveNav("#about")
+    // ) : position3 === 0 ? (
+    //   setActiveNav("#experience")
+    // ) : position4 === 0 ? (
+    //   setActiveNav("#services")
+    // ) : position5 === 0 ? (
+    //   setActiveNav("#contact")
+    // ) : (
+    //   <></>
+    // );
+  });
+
   return (
     <nav>
       <a
